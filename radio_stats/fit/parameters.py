6a157055db49e50d86357350f13bff6a4d8671e5
Christian Franz Wolfgang Arauner <christian.arauner@tu-dortmund.de>
2024-07-15 12:29:21 +0200

    added function to fit a linear function on the intensities and distances

---
+++ radio_stats/fit/parameters.py
@@ -0,0 +1,54 @@
+from radio_stats.reconstruct.gaussians import reconstruct_gauss
+from radio_stats.stats.parameters import calculate_distances, calculate_offset
+import numpy as np
+from scipy.optimize import curve_fit
+
+def fit_lin(_parameters) -> tuple[np.ndarray, np.ndarray]:
+    '''
+    Calculate parameters and errors of linear regression for intensity and brightness of fitted
+    gaussians.
+
+    Parameters
+    ----------
+    _parameters : array_like
+        Parameters to be fitted
+
+    Returns
+    -------
+    params : np.ndarray
+        Fitted parameters for the linear function
+
+    errors : np.ndarray
+        Errors of the fitted parameters
+    '''
+    try:
+        _parameters[np.argwhere(np.abs(_parameters[:,2:4]) > 1024)[:,0],0] = 0
+        parameters = _parameters[:np.min(np.argwhere(_parameters[:,0] == 0))]
+    except ValueError:
+        parameters = _parameters
+    # if only one point is fittet, we can't fit a x1 function
+    if parameters.shape[0] <= 1:
+        return np.array([-np.inf,-np.inf]), np.array([-np.inf,-np.inf])
+    
+    image = reconstruct_gauss(parameters, (1024, 1024))
+    
+    distances = calculate_distances(parameters)
+    
+    x, y = calculate_offset(parameters, image) 
+        
+    try:
+        flux = image[y,x]
+    except IndexError as e:
+        print("IndexError:", e)
+        return np.array([-np.inf,-np.inf]), np.array([-np.inf,-np.inf])
+        
+    def x1(x, a, b) -> float:
+        return a * x + b
+
+    params, covmat = curve_fit(x1, distances, np.log(flux), maxfev=10000)
+    errors = np.sqrt(np.diag(covmat))
+    
+    if parameters.shape[0] == 2 and np.all(errors == np.inf):
+        errors = np.array([0,0])
+            
+    return params, errors
