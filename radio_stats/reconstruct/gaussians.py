28dd4832efbc804b29f62f3e3328137dd05098ba
Christian Arauner <christian.arauner@tu-dortmund.de>
2024-10-07 10:51:26 +0200

    added verbosity option

--- radio_stats/reconstruct/gaussians.py
+++ radio_stats/reconstruct/gaussians.py
@@ -2,7 +2,7 @@ import numpy as np
 from gaussfitter import twodgaussian
 
 
-def reconstruct_gauss(parameters: list, shape: tuple, **kwargs) -> np.ndarray:
+def reconstruct_gauss(parameters: list, shape: tuple, verbose=False, **kwargs) -> np.ndarray:
     """
     Generate a centered image out of parameters for gaussians.
 
@@ -27,16 +27,18 @@ def reconstruct_gauss(parameters: list, shape: tuple, **kwargs) -> np.ndarray:
         parameters = [parameters]
     peak = np.floor_divide(shape, 2)
     gaussians = []
-    for params in parameters:
+    summed_gaussian = np.zeros(shape)
+    if verbose:
+        from tqdm import tqdm
+        iterator = tqdm(parameters)
+    else:
+        iterator = parameters
+        
+    for params in iterator:
         params[2:4] += int(shape[0]/2)
         if np.all(params == 0):
             continue
-        gaussians.append(twodgaussian(params, **kwargs)(*np.indices(shape)))
-    gaussians = np.array(gaussians)
-    if len(gaussians.shape) == 2:
-        summed_gaussian = gaussians
-    else:
-        summed_gaussian = gaussians.sum(axis=0)
+        summed_gaussian += twodgaussian(params, **kwargs)(*np.indices(shape))
 
     difference = peak - np.argwhere(summed_gaussian == np.max(summed_gaussian))[0]
 
